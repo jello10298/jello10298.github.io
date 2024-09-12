fe83023b75857a47d59d2e86539adbcf4fd784ec
Angelo Gonzalez <angelogonzalez@outlook.com>
2024-09-11 19:50:52 -0500

    Portfolio Update

--- src/Portfolio/tilesData.js
+++ src/Portfolio/tilesData.js
@@ -8,13 +8,15 @@ export const defaultImages = [
 
 export const tilesData = [
     {
-        name: 'United Health',
+        name: 'United Healthcare',
         src: "/portfolio/Grid/UHC.jpg",
         width: 622,
         height: 361,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Implemented Find and Price Care for United Healthcare after being transitioned through a corporate acquisition from Rally Health into Optum, the digital arm of United Healthcare.',
         images: [
             '/portfolio/UHC.png',
+            '/portfolio/uhc1.png',
+            '/portfolio/uhc2.png',
             '/portfolio/RallyHealth.png',
         ]
     },
@@ -23,8 +25,10 @@ export const tilesData = [
         src: "/portfolio/Grid/Hyatt.jpg",
         width: 503,
         height: 360,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Phase I - Rewrote the frontend of over 1,600 property based websites, one for each hotel, for Hyatt Hotels.  Phase II - Converted the AEM components into React components on a NextJS micro-architecture leveraging RestAPIs powered by Apollo GraphQL.',
         images: [
+            '/portfolio/Hyatt/andaz.png',
+            '/portfolio/Hyatt/thompson.png',
             '/portfolio/Hyatt/chiph.png',
             '/portfolio/Hyatt/grand-hyatt.png',
             '/portfolio/Hyatt/hyatt-regency.png',
@@ -36,18 +40,16 @@ export const tilesData = [
             '/portfolio/Hyatt/urcove.png',
             '/portfolio/Hyatt/miraval.png',
             '/portfolio/Hyatt/alila.png',
-            '/portfolio/Hyatt/andaz.png',
-            '/portfolio/Hyatt/thompson.png',
         ]
     },
     // {src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg", width: 320, height: 212,},
     // {src: "https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg", width: 270, height: 180,},
     {
-        name: 'Microsoft',
+        name: 'Microsoft, Inc.',
         src: "/portfolio/Grid/Microsoft.jpg",
         width: 502,
         height: 362,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: `Implemented "Buy Box" AEM component (where the product information and buy button live), built the frontend for Xbox remote game installation from microsoft.com to your console, conducted Gatling load testing for microsoft.com and built a constellation app for the IE11 demo in San Francisco, highlighting the performance capacity of the <canvas /> tag (at that time, the Flash killer)`,
         images: [
             '/portfolio/Microsoft - Buy Box.png',
             '/portfolio/Microsoft - Remote Install.png',
@@ -55,11 +57,11 @@ export const tilesData = [
         ]
     },
     {
-        name: 'Valtech',
+        name: 'valtech_',
         src: "/portfolio/Grid/Valtech.jpg",
         width: 708,
         height: 358,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Created a marketing page for Rolex, built a site for Hyatt Meetings & Events and built an Adobe Experience Manager Digital Asset Manager tool for Lundbeck and Hyatt.',
         images: [
             '/portfolio/Rolex.png',
             '/portfolio/HyattEvents.png',
@@ -71,7 +73,7 @@ export const tilesData = [
         src: "/portfolio/Grid/AT&T.jpg",
         width: 486,
         height: 361,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'While at the Chief Security Office, fixed performance bugs in the Customer Policy Tuning application (to aid in IPv4 to IPv6 transitions), assisted with "Bring Your Own Device" to work initiative (multiple profiles on one device) and pen-tested Fortinet/Palo Alto networks for security vulnerabilities and corporate configuration management.',
         images: [
             '/portfolio/att.webp',
         ]
@@ -81,21 +83,23 @@ export const tilesData = [
         src: "/portfolio/Grid/ChicagoVentures.jpg",
         width: 335,
         height: 359,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Introduced to Blitsy, an Arts & Crafts repackaging company, after an acquisition attempt of Vapor4Life, where I managed a Magento e-commerce store (now called Adobe Commerce) for one of the first electronic cigarette companies introduced in America.',
         images: [
-            '/portfolio/poggled.png',
+            '/portfolio/blitsy2.png',
+            // '/portfolio/blitsy.jpg',
             '/portfolio/archive2/3.v4l.jpg',
             '/portfolio/archive2/1.wowvapor.jpg',
             '/portfolio/archive2/2.wowvapor.jpg',
         ]
     },
+
     // {src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg", width: 139, height: 180,},
     {
         name: 'Case/New Holland',
         src: "/portfolio/Grid/CNH.jpg",
         width: 638,
         height: 361,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Built a Quality Assurance dashboard for Burr Ridge satellite office of Case/New Holland to aid in the tracking of manufacturing line defects of industrial farming equipment for the purpose of dialing-in design process and warranty information.',
         images: [
             '/portfolio/cnh3.webp',
             '/portfolio/cnh1.jpg',
@@ -107,7 +111,7 @@ export const tilesData = [
         src: "/portfolio/Grid/Firestone.jpg",
         width: 724,
         height: 362,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Contracted through Two x Four to build subsidiary branded websites for recent Firestone acquisitions of Wheel Works and Hibdon Tires.',
         images: [
             '/portfolio/archive2/6.wheelworks.jpg',
             '/portfolio/archive2/7.hibdon.jpg',
@@ -118,7 +122,7 @@ export const tilesData = [
         src: "/portfolio/Grid/Rolex.jpg",
         width: 479,
         height: 362,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Brought on-board to build a marketing website for Rolex, a luxury watch maker, through advertising agency Valtech_, located in West Town, Chicago. Leveraging the latest in design construction methodology for a beautiful website.',
         images: [
             '/portfolio/Rolex.png',
         ]
@@ -128,7 +132,7 @@ export const tilesData = [
         src: "/portfolio/Grid/Dominos.jpg",
         width: 270,
         height: 180,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: `Built over 300 LSM (local store marketing) micro-sites for Domino's Pizza franchise's throughout North America, helping to establish Domino's Pizzas brand dominance before online ordering for pizza was available (i.e. Pre - Domino's Pizza Tracker).`,
         images: [
             '/portfolio/dominos/1.unt.jpg',
             '/portfolio/dominos/2.uconn.jpg',
@@ -157,7 +161,7 @@ export const tilesData = [
         src: "/portfolio/Grid/Discover.jpg",
         width: 318,
         height: 180,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'ETL developer charged with building the first ETL administration interface for loading terabytes of customer data from multiple data sources into one enterprise data warehouse using the fastest loading tools available.',
         images: [
             '/portfolio/Discover.png',
         ]
@@ -167,7 +171,7 @@ export const tilesData = [
         src: "/portfolio/Grid/RIC.jpg",
         width: 318,
         height: 180,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Administered the journal submission site for IEEE Engineering in Medicine and Biology Society publications.',
         images: [
             '/portfolio/IEEE - EMBS.png',
         ]
@@ -177,7 +181,7 @@ export const tilesData = [
         src: "/portfolio/Grid/iCAIR.jpg",
         width: 724,
         height: 359,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Studied and maintained Internet2 approaches for optimizing video data delivery during a time when the internet was geared towards text and image data delivery only.  Paving the way for optimized video compression algorithms for online video streaming.',
         images: [
             '/portfolio/iCAIR.png',
         ]
@@ -189,7 +193,7 @@ export const tilesData = [
         src: "/portfolio/Grid/MRC-alt-2.jpg",
         width: 719,
         height: 358,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Helped build education oriented websites for teaching Material Science to high school students.',
         images: [
             '/portfolio/InternationalVirtualInstitute.png',
         ]
@@ -199,7 +203,7 @@ export const tilesData = [
         src: "/portfolio/Grid/Blitsy.jpg",
         width: 335,
         height: 359,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Maintained and improved the efficiency of a Magento e-commerce for Blitsy, an Arts & Crafts startup company, at the time, the most well funded startup in Chicago.',
         images: [
             '/portfolio/blitsy3.jpg',
             '/portfolio/blitsy2.png',
@@ -210,25 +214,35 @@ export const tilesData = [
         src: "/portfolio/Grid/Wellpoint.jpg",
         width: 318,
         height: 180,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        desc: 'Redesigned the corporate website for American Imaging Management, a radiology tracking company that handled all claims authorizations for Wellpoint.',
         images: [
             '/portfolio/archive2/4.aim.jpg',
         ]
     },
     {
-        name: 'Porsche',
-        src: "/portfolio/Grid/Porsche.jpg",
-        width: 337,
-        height: 361,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        name: 'lightbank',
+        src: "/portfolio/Grid/Lightbank.png",
+        width: 400,
+        height: 166,
+        desc: 'Small contract to build out an administration panel for Poggled, lead by Northwestern Kellogg graduates Joesph Matthews and Sean Strother, a Groupon style platform for entertainment venues.',
+        images: [
+            '/portfolio/poggled.png',
+        ]
     },
-    {
-        name: 'Ferrari',
-        src: "/portfolio/Grid/Ferrari.jpg",
-        width: 337,
-        height: 361,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
+    // {
+    //     name: 'Porsche',
+    //     src: "/portfolio/Grid/Porsche.jpg",
+    //     width: 337,
+    //     height: 361,
+    //     desc: 'Coming soon, ',
+    // },
+    // {
+    //     name: 'Ferrari',
+    //     src: "/portfolio/Grid/Ferrari.jpg",
+    //     width: 337,
+    //     height: 361,
+    //     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // },
 // { src: "/portfolio/Grid/MRC.jpg", },
 // { src: "/portfolio/Grid/MRC-alt.jpg", },
 ];
